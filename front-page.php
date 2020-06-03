bdc5ccb1905226053ddbf9ba02120aa5cbae3d1b
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-06-03 10:57:44 +0900

    記事一覧途中

--- front-page.php
+++ front-page.php
@@ -2,9 +2,166 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
-<section class="py-5">
-<div class="container">
-
+<section id="new" class="article-list py-5">
+<div class="wrap">
+  <div class="ttl flex">
+    <h2><i class="far fa-clock"></i>新着物件</h2>
+    <div class="btn-02">
+      <a href="#">一覧を見る<i class="fas fa-chevron-right"></i></a>
+    </div>
+  </div>
+  <ul class="flex">
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+  </ul>
 </div>
 </section>
+
+<section id="ranking" class="article-list py-5">
+<div class="wrap">
+  <div class="ttl flex">
+    <h2><i class="far fa-clock"></i>人気物件情報</h2>
+    <div class="btn-02">
+      <a href="#">一覧を見る<i class="fas fa-chevron-right"></i></a>
+    </div>
+  </div>
+  <ul class="flex">
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+    <!--- 物件情報 --->
+    <li>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/sample/sample.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <a class="cat">名古屋駅</a>
+        <h3>オフィス名がはいりますオフィス名がはいります</h3>
+        <p>面積：8坪
+        <br>所在地：中村区椿町
+        <br>賃料：</p>
+        <p class="price">6万5000円</p>
+      </div>
+    </li>
+    <!--- 物件情報終わり --->
+  </ul>
+</div>
+</section>
+
+
 <?php get_footer();
--- header.php
+++ header.php
@@ -6,6 +6,13 @@ $wp_url = get_template_directory_uri(); ?>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link href="<?php echo $wp_url; ?>/lib/css/common.css" rel="stylesheet">
+<link href="<?php echo $wp_url; ?>/lib/css/header.css" rel="stylesheet">
+<link href="<?php echo $wp_url; ?>/lib/css/footer.css" rel="stylesheet">
+<link href="<?php echo $wp_url; ?>/lib/css/mv.css" rel="stylesheet">
+<link href="<?php echo $wp_url; ?>/lib/css/top.css" rel="stylesheet">
+<link href="<?php echo $wp_url; ?>/lib/css/article.css" rel="stylesheet">
+<link href="<?php echo $wp_url; ?>/lib/css/single.css" rel="stylesheet">
 <?php wp_head(); ?>
 <?php if (!is_user_logged_in()): ?>
 <!-- ここにGAトラッキングタグ -->
@@ -15,42 +22,109 @@ $wp_url = get_template_directory_uri(); ?>
 
 <!-- ヘッダー -->
 <header id="header">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <a class="navbar-brand" href="<?php echo $home; ?>/">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="<?php echo $home; ?>/">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+  <nav class="wrap">
+    <div class="flex">
+      <div class="logo-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/common/header_logo.png" alt="10万円以下ドットコムのロゴ">
+      </div>
+      <div class="flex pc-only">
+        <img src="<?php echo $wp_url ?>/lib/images/common/header_tel.png" alt="10万円以下ドットコムのロゴ">
+        <div class="btn-01">
+          <a href="#" target="_blank">資料請求／お問い合わせ<i class="fas fa-arrow-circle-right"></i></a>
+        </div>
       </div>
     </div>
   </nav>
 </header>
 <!-- ヘッダー終了 -->
+
+<?php if (is_home() || is_front_page()): ?>
+
+<!-- トップページMV -->
+<section id="mv" class="flex flex-center">
+  <div class="wrap flex">
+    <h2 class="left">
+      <img src="<?php echo $wp_url ?>/lib/images/mv/mv_txt.png" alt="名古屋で安い事務所やオフィス、10万円以下の賃貸はお任せ！">
+    </h2>
+    <div class="right">
+      <h3><i class="fas fa-search"></i>こだわり検索</h3>
+      <ul class="flex prejudice-list">
+        <li>
+          <a href="#">
+            <p>レンタル<br>オフィス物件</p>
+            <div class="">
+              <i class="fas fa-arrow-circle-right"></i>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <p>シェア<br>オフィス物件</p>
+            <div class="">
+              <i class="fas fa-arrow-circle-right"></i>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <p>敷金礼金なし<br>物件</p>
+            <div class="">
+              <i class="fas fa-arrow-circle-right"></i>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <h3><i class="fas fa-search"></i>エリアから検索</h3>
+      <ul class="area-list flex">
+        <li><a href="#">名古屋駅<i class="fas fa-chevron-right"></i></a></li>
+        <li><a href="#">栄<i class="fas fa-chevron-right"></i></a></li>
+        <li><a href="#">丸の内<i class="fas fa-chevron-right"></i></a></li>
+        <li><a href="#">伏見<i class="fas fa-chevron-right"></i></a></li>
+        <li><a href="#">久屋大通り<i class="fas fa-chevron-right"></i></a></li>
+        <li><a href="#">金山<i class="fas fa-chevron-right"></i></a></li>
+        <li><a href="#">その他<i class="fas fa-chevron-right"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</section>
+<!-- トップページMV終了 -->
+
+<?php else: ?>
+
+<!-- 下層ページMV -->
+<section id="sub-mv">
+  <!-- パンくず -->
+  <?php if (is_home() || is_front_page()): ?>
+  <?php else: ?>
+  <?php if (!is_front_page() && !is_home()) {
+  if (function_exists('yoast_breadcrumb')) {
+      yoast_breadcrumb('<div id="breadcrumbs" class="pc">','</div>');
+    }
+  } ?>
+  <?php endif; ?>
+  <?php
+  global $wp_query;
+  $post_obj = $wp_query->get_queried_object();
+  $slug = $post_obj->post_name;
+  ?>
+  <!-- パンくず終了 -->
+  <div class="flex flex-center">
+    <div class="content txt-c">
+      <?php if (is_post_type_archive('news')): ?>
+      <h2 class="serif fff">ニュース</h2>
+    <?php elseif (is_post_type_archive('research')): ?>
+      <h2 class="serif fff">研究コラム・成果</h2>
+    <?php else: ?>
+      <h2 class="serif fff"><?php echo get_the_title(); ?></h2>
+      <?php endif; ?>
+      </h2>
+      <!-- <p class="eng fff"><?php echo $slug; ?></p> -->
+    </div>
+  </div>
+</section>
+<!-- 下層ページMV終了 -->
+
+<?php endif; ?>
 
 <!-- メインコンテンツ -->
 <main>
